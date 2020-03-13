c6a2f0e5cfe021395df6835723ec95d755a734a8
90ngrove <54635260+90ngrove@users.noreply.github.com>
2020-03-12 20:19:57 -0400

    Update logic2.js

--- static/js/logic2.js
+++ static/js/logic2.js
@@ -7,7 +7,7 @@ function buildCharts(sample) {
     var topValues = resultArray.sort((a,b) => b.issue_date.length-a.issue_date.length).slice(0,10);
     var song = topValues.map(row => row.title)
     var vader = topValues.map(row => row.VADER)
-    var layout = {barmode: 'relative'};
+    var layout = {barmode: 'relative', title: `Sentiment Scores for Top 10 Songs from the ${sample}`,margin: { t: 75, b: 150 } };
     // { title: `${sample}'s Hit Songs and Number of Weeks at #1`,margin: { t: 50, b: 200 } };
     var data = [
       {
@@ -31,60 +31,36 @@ function buildCharts(sample) {
       insidetextorientation: "radial"
     }]
     
-    var layout = [{
-      height: 700,
-      width: 700
-    }]
+    var layout = {title: `Sentiment Scores for all Songs from the ${sample}`,
+      height: 400,
+      width: 1500
+    }
     
     Plotly.newPlot('BarChart', data, layout)
     
-    // ////Group songs based on title to remove the duplicates
-    // var output = [];
-
-    // resultArray.forEach(function(item) {
-    //   var existing = output.filter(function(v, i) {
-    //     return v.title == item.title;
-    //   });
-    //   if (existing.length) {
-    //     var existingIndex = output.indexOf(existing[0]);
-    //     output[existingIndex].issue_date = output[existingIndex].issue_date.concat(item.issue_date);
-    //   } else {
-    //     if (typeof item.issue_date == 'string')
-    //       item.issue_date = [item.issue_date];
-    //     output.push(item);
-    //   }
-    // });
-    
-
-
-
-    
-    // var layout2 = { title: `${sample}'s Hit Songs Over Time`,margin: { t: 50, b: 200 } };
-    // var desired_maximum_marker_size = 40;   
-    // var length2 = output.map(row => row.issue_date.length)
-    // var size = length2;
-    // var year2 = output.map(row => row.year);
-    // var data2 = [
-    //   {
-    //     x: year2,
-    //     y: length2,
-    //     mode: "markers",
-    //     marker: {
-    //       size: size,
-    //       sizeref: 2.0 * Math.max(...size) / (desired_maximum_marker_size**2),
-    //       sizemode: 'area'
-    //   }}];
-    
-    // Plotly.newPlot("BarChart", data2, layout2);
 
     
   });
 }
 
-// var changedElement = d3.select(".vertical-menu").on("click");
-// var elementValue = changedElement.property("value");
-// var element = attr("id").on("click",buildCharts);
-// d3.select("#id").on("click",buildCharts);
+function wordcloud(sample) {
+  // // This code works save for later
+  $(document).ready(function () {
+    // on page load this will fetch data from our flask-app asynchronously
+   $.ajax({url: '/wordcloud', success: function (data) {
+       // returned data is in string format we have to convert it back into json format
+       var words_data = $.parseJSON(data);
+       var redwords_data = words_data.filter(sampleObj => sampleObj.decade == sample);
+       // we will build a word cloud into our div with id=word_cloud
+       // we have to specify width and height of the word_cloud chart
+       $('#wordcloud').html("");
+       $('#wordcloud').jQCloud(redwords_data, {
+           width: 800,
+           height: 600
+       });
+   }});
+  });
+  }
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -124,13 +100,17 @@ function init() {
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
+    wordcloud(firstSample)
   });
 }
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
+  wordcloud(newSample)
 }
 
 // Initialize the dashboard
 init();
+
+
